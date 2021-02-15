7386acd9dedbc2d0d7856a4cdfc5f303b84d68c0
alexander.peter <alexander.peter@stud.hslu.ch>
2021-02-15 17:02:19 +0100

    create first working api

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,5 +1,13 @@
 # manual
 
 npm install
-amplify init
+npm install -g @aws-amplify/cli
+
+// powershell
+amplify configure
+// configure aws credentials
+
+amplify init // (once per environment necessary)
 amplify push
+
+http://localhost:8080
--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -4,30 +4,16 @@ import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
-function callApi(method, data) {
-    if (!(method in ["get, post"])) {
-        console.error(`Invalid API call: ${method}`)
-        return;
-    }
-
-    const apiName = 'restapi';
-    const path = '/items';
-    return await API[method](apiName, path, data);
-}
-
-function get(data) {
-    return callApi("get", data);
-}
-
-function post(data) {
-    return callApi("post", data);
+async function uploadImage(data) {
+    const apiName = 'cplabWebappApi';
+    const path = '/image';
+    return await API.post(apiName, path, data);
 }
 
 const MutationButton = document.getElementById("MutationEventButton");
 const MutationResult = document.getElementById("MutationResult");
 
-MutationButton.addEventListener("click", (evt) => {
-    createNewTodo().then((evt) => {
-        MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
-    });
+MutationButton.addEventListener("click", async () => {
+    const event = await uploadImage();
+    MutationResult.innerHTML += `<p>${event}</p>`;
 });
