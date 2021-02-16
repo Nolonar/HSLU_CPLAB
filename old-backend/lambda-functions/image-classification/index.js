0bc2fe3d283f8de4e5c8584311b1dc319240c4aa
alexander.peter <alexander.peter@stud.hslu.ch>
2021-02-16 20:29:16 +0100

    fix image upload with base64

--- old-backend/lambda-functions/image-classification/index.js
+++ old-backend/lambda-functions/image-classification/index.js
@@ -1,27 +1,36 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
-exports.handler = async (event) => {
+function unwrapImage(formData) {
+    const lines = formData.split(/\r?\n/);
+    for (let i = 0; i < lines.length; i++) {
+        console.log("line" + i + ": " + lines[i]);
+    }
+
+    const fileName = Date.now().toString(36) + "_" + lines[7];
+    const body = lines[3];
+    const typeMatch = body.match(/data:(.*?);/gi);
+    const contentType = (0 < typeMatch.length) ? typeMatch[0].slice(5, -1) : "text/plain";
+
+    return { fileName, contentType, body }
+}
+
+exports.handler = async(event) => {
     // console.log('request: ' + JSON.stringify(event))
     const response = {
         statusCode: 500,
         body: '',
     }
     try {
-        const payload = Buffer.from(event.body, 'base64').toString();
-
-        console.log('request: ' + payload.substr(40, 100));
-
-        const randName = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-        console.log(randName)
+        const imageObject = unwrapImage(event.body);
 
         const bucketParams = {
             Bucket: 'classification-image-gallery',
-            Key: randName,
-            Body: payload,
-            ContentType: "image"
+            Key: imageObject.fileName,
+            Body: imageObject.body,
+            ContentType: imageObject.contentType
         };
-
+        console.log('bucketParams: ' + JSON.stringify(bucketParams));
         await s3.putObject(bucketParams).promise();
 
     } catch (err) {
--- webapp/src/app.js
+++ webapp/src/app.js
@@ -6,24 +6,33 @@ Amplify.configure(awsconfig);
 async function postForm(path, formdata) {
     const apiName = 'cplabWebappApi';
     for (const pair of formdata.entries()) {
-        console.log('formdata: ' + pair[0] + ', ' + pair[1])
+        console.log('formdata: ' + pair[0] + ', ' + JSON.stringify(pair[1]))
     }
-
     return await API.post(apiName, path, { body: formdata });
 }
 
 const response = document.getElementById("response");
-
+const imageInput = document.getElementById("image");
+const fileInput = document.getElementById("file");
 const uploadForm = document.getElementById("uploadForm");
-uploadForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
+const checkForm = document.getElementById("checkForm");
 
+fileInput.addEventListener("change", () => {
+    const filReader = new FileReader();
+
+    filReader.addEventListener("load", () => {
+        imageInput.value = filReader.result;
+    });
+    filReader.readAsDataURL(fileInput.files[0]);
+});
+
+uploadForm.addEventListener("submit", async(event) => {
+    event.preventDefault();
     const result = await postForm("/image", new FormData(uploadForm));
     response.innerHTML += `<p>${JSON.stringify(result)}</p>`;
 });
 
-const checkForm = document.getElementById("checkForm");
-checkForm.addEventListener("submit", async (event) => {
+checkForm.addEventListener("submit", async(event) => {
     event.preventDefault();
 
     const correctness = {
