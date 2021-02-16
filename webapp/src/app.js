aba9414059a184baf6cefa6192a59e7606a19091
Léonard Wyrsch <leonard.wyrsch@stud.hslu.ch>
2021-02-16 17:05:14 +0100

    add form and style

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -3,44 +3,37 @@ import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
-async function uploadImage(formdata) {
+async function postForm(path, formdata) {
     const apiName = 'cplabWebappApi';
-    const path = '/image';
-    const data = {
-        body: formdata
+    for (const pair of formdata.entries()) {
+        console.log('formdata: ' + pair[0] + ', ' + pair[1])
     }
-    return await API.post(apiName, path, data);
+
+    return await API.post(apiName, path, { body: formdata });
 }
 
-async function saveChecking(formdata) {
-    const apiName = 'cplabWebappApi';
-    const path = '/checking';
-    const data = {
-        body: {
-            "image_id": "lena.jpg",
-            "user_id": "michael.jackson",
-            "correctness": true
-        }
-    }
-    console.log('data: ' + JSON.stringify(data));
-    return await API.post(apiName, path, data);
-}
-
-const MutationResult = document.getElementById("MutationResult");
+const response = document.getElementById("response");
 
 const uploadForm = document.getElementById("uploadForm");
 uploadForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    const formdata = new FormData(uploadForm);
-    for (const pair of formdata.entries()) {
-        console.log('formdata: ' + pair[0] + ', ' + pair[1])
-    }
-    const result = await uploadImage(formdata);
-    MutationResult.innerHTML += `<p>${JSON.stringify(result)}</p>`;
+
+    const result = await postForm("/image", new FormData(uploadForm));
+    response.innerHTML += `<p>${JSON.stringify(result)}</p>`;
 });
 
-const MutationButton2 = document.getElementById("MutationEventButton2");
-MutationButton2.addEventListener("click", async () => {
-    const event = await saveChecking();
-    MutationResult.innerHTML += `<p>${JSON.stringify(event)}</p>`;
+const checkForm = document.getElementById("checkForm");
+checkForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const correctness = {
+        Yes: true,
+        No: false
+    }[event.submitter.value];
+
+    const formdata = new FormData(checkForm);
+    formdata.append("correctness", correctness);
+
+    const result = await postForm("/checking", formdata);
+    response.innerHTML += `<p>${JSON.stringify(result)}</p>`;
 });
