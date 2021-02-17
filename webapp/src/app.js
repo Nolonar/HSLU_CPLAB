d800295afe93b5859651936a161f0505b80a83f8
Léonard Wyrsch <leonard.wyrsch@stud.hslu.ch>
2021-02-17 23:10:10 +0100

    implement statistics

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -3,6 +3,8 @@ import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
+const apiName = 'cplabWebappApi';
+
 const loadingOverlay = document.getElementById("loadingOverlay");
 const loginPanel = document.getElementById("loginPanel");
 const loginForm = document.getElementById("loginForm");
@@ -26,11 +28,20 @@ const originalImage = document.getElementById("originalImage");
 const classification = document.getElementById("classification");
 const checkForm = document.getElementById("checkForm");
 const imageId = document.getElementById("imageId");
+const overallCorrect = document.getElementById("overallCorrect");
+const overallIncorrect = document.getElementById("overallIncorrect");
+const overallAccuracy = document.getElementById("overallAccuracy");
+const userCorrect = document.getElementById("userCorrect");
+const userIncorrect = document.getElementById("userIncorrect");
+const userAccuracy = document.getElementById("userAccuracy");
 const response = document.getElementById("response");
 
 let currentUser = null;
 
-Auth.currentAuthenticatedUser().then(user => setCurrentUser(user)).catch(error => console.log(error));
+Auth.currentAuthenticatedUser().then(user => {
+    setCurrentUser(user);
+    updateStatistics();
+}).catch(error => console.log(error));
 
 function setCurrentUser(user) {
     console.log(user);
@@ -70,7 +81,8 @@ async function signIn() {
     showLoadingOverlay(async () => {
         try {
             setCurrentUser(await Auth.signIn(username.value, password.value));
-            console.log("Login successful")
+            updateStatistics();
+            console.log("Login successful");
         } catch (error) {
             logLoginError(error);
         }
@@ -111,6 +123,8 @@ async function signOut() {
     showLoadingOverlay(async () => {
         try {
             await Auth.signOut();
+            currentUser = null;
+
             loginPanel.classList.remove("hidden");
             mainPanel.classList.add("hidden");
             username.value = "";
@@ -121,8 +135,38 @@ async function signOut() {
     });
 }
 
+function getAccuracy(correct, incorrect) {
+    const total = correct + incorrect;
+    return total ? (100 * correct / total).toFixed(0) : 0;
+}
+
+async function updateStatistics() {
+    if (!currentUser) {
+        return;
+    }
+
+    let statistics = null;
+    try {
+        statistics = await API.get(apiName, "/statistic", {
+            queryStringParameters: {
+                username: currentUser.attributes.email
+            }
+        });
+        console.log(statistics);
+    } catch (error) {
+        logError(error);
+        return;
+    }
+    overallCorrect.innerText = statistics.correctOverall;
+    overallIncorrect.innerText = statistics.wrongOverall;
+    overallAccuracy.innerText = `${getAccuracy(statistics.correctOverall, statistics.wrongOverall)}%`;
+
+    userCorrect.innerText = statistics.correctUser;
+    userIncorrect.innerText = statistics.wrongUser;
+    userAccuracy.innerText = `${getAccuracy(statistics.correctUser, statistics.wrongUser)}%`;
+}
+
 async function postForm(path, formdata) {
-    const apiName = 'cplabWebappApi';
     for (const pair of formdata.entries()) {
         console.log('formdata: ' + pair[0] + ', ' + JSON.stringify(pair[1]))
     }
@@ -196,6 +240,8 @@ checkForm.addEventListener("submit", async (event) => {
     formdata.append("userId", currentUser.attributes.email);
     formdata.append("correctness", correctness);
     const result = await postForm("/checking", formdata);
+    updateStatistics();
+
     fileInput.value = "";
     classifyPanel.classList.remove("hidden");
     checkPanel.classList.add("hidden");
