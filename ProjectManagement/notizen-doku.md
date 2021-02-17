7e411393a990d6dfefc601734d80fdfd5113f8e2
alexander.peter <alexander.peter@stud.hslu.ch>
2021-02-17 14:05:53 +0100

    add rekognition call

--- ProjectManagement/notizen-doku.md
+++ ProjectManagement/notizen-doku.md
@@ -1,12 +1,21 @@
 
-# Lambda
-- Full access via pull 
+# Lambda / IAM
+- S3FullAccess via pull 
+- AmazonRekognitionFullAccess 
+- reicht Memory?
+- reicht 3 Sekunden Timeout?
 
-# Spezielles/erwähnenswert
+# S3 > bucket
 - Der S3-Bucket muss manuell erstellt werden: der Name muss 'classification-image-gallery' sein
+- Die Region ist 'us-east-1'
 
 # User management
 - Benutzer manuell erstellt
 
 
 Die Annahmen sind in der Dokumentation zu beschreiben.
+
+Wozu gibt es die event.json?
+- Sind die als testevent vorgesehen?
+
+Wieso bekommt das frontend von der API nur null zurück?
--- webapp/amplify/backend/function/classificateImage/src/index.js
+++ webapp/amplify/backend/function/classificateImage/src/index.js
@@ -1,21 +1,20 @@
 const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01', region: 'us-east-1' });
+const rekognition = new aws.Rekognition();
+const bucketName = 'classification-image-gallery';
 
 function unwrapImage(formData) {
     const lines = formData.split(/\r?\n/);
-    for (let i = 0; i < lines.length; i++) {
-        console.log("line" + i + ": " + lines[i]);
-    }
-
-    const fileName = Date.now().toString(36) + "_" + lines[7];
-    const body = lines[3];
-    const typeMatch = body.match(/data:(.*?);/gi);
+    const parts = lines[3].split(',');
+    const typeMatch = parts[0].match(/data:(.*?);/gi);
     const contentType = (0 < typeMatch.length) ? typeMatch[0].slice(5, -1) : "text/plain";
+    const fileName = Date.now().toString(36) + "_" + lines[7] + "." + contentType.split("/")[1];
+    const decoded_data = Buffer.from(parts[1], 'base64');
 
-    return { fileName, contentType, body }
+    return { fileName, contentType, decoded_data }
 }
 
-exports.handler = async(event) => {
+exports.handler = async (event) => {
     // console.log('request: ' + JSON.stringify(event))
     const response = {
         statusCode: 500,
@@ -27,22 +26,35 @@ exports.handler = async(event) => {
     }
     try {
         const imageObject = unwrapImage(event.body);
+        const filename = imageObject.fileName;
 
-        const decoded = Buffer.from(imageObject.body.split(',')[1], 'base64');
-
-        const bucketParams = {
-            Bucket: 'classification-image-gallery',
-            Key: imageObject.fileName,
-            Body: decoded,
+        const savingParams = {
+            Bucket: bucketName,
+            Key: filename,
+            Body: imageObject.decoded_data,
             ContentType: imageObject.contentType
         };
-        console.log('bucketParams: ' + JSON.stringify(bucketParams));
-        await s3.putObject(bucketParams).promise();
+        console.log('bucketParams: ' + JSON.stringify(savingParams));
+        await s3.putObject(savingParams).promise();
+
+        const classificationParams = {
+            Image: {
+                S3Object: {
+                    Bucket: bucketName,
+                    Name: filename
+                }
+            },
+            MaxLabels: 1,
+            MinConfidence: 50
+        };
+        const result = await rekognition.detectLabels(classificationParams).promise();
+        const category = (0 < result.Labels.length) ? result.Labels[0].Name : "undefined";
+
         response.statusCode = 200;
-        response.body = "success";
+        response.body = { filename, category };
 
     } catch (err) {
-        response.body = 'err: ' + JSON.stringify(err);
+        response.body = { err };
     }
     console.log('response: ' + JSON.stringify(response))
     return response;
